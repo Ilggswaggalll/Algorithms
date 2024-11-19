ff0d0a3a15911b959629b88b6c89821e575d8eda
Ilggswaggalll <162141227+Ilggswaggalll@users.noreply.github.com>
2024-11-19 09:17:29 +0300

    commit

--- Workbook 3/Task_1.py
+++ Workbook 3/Task_1.py
@@ -1,5 +1,14 @@
 import datetime
 
+
+# сравнению чисел
+def srav(a, b):
+    for i in range(len(a)):
+        if a[i] < b[i]:
+            return 1
+    return 0
+
+
 # вычитание в столбик подготовленных
 def subt(m1, m2, a):
     m3 = [0] * len(m1)
@@ -15,6 +24,7 @@ def subt(m1, m2, a):
                 else:
                     m1[j] -= 1
                     break
+            
             m1[i] += 10
             m3[i] = m1[i] - m2[i]
         else:
@@ -26,72 +36,39 @@ def subt(m1, m2, a):
             break
     for i in range(h, len(m3)):
         m.append(m3[i])
+    if m == [0]:
+        return m[1:]
     return m
 
 
 # вычитание чисел в столбик
 def subtraction(a, b):
-    if int(a) < 0:
-        print()
-        if len(a[1:]) != len(b):
-            if len(a[1:]) > len(b):
-                m1 = [int(i) for i in a[1:]]
-                print(m1)
+        if len(a) != len(b):
+            if len(a) > len(b):
+                m1 = [int(i) for i in a]
                 m2 = []
-                for i in range(len(a[1:]) - len(b)):
+                for i in range(len(a) - len(b)):
                     m2.append(0)
                 for i in b:
                     m2.append(int(i))
-                print(m1)
-                print(m2)
                 m = subt(m1, m2, 0)
                 return m
             else:
                 m2 = [int(i) for i in b]
                 m1 = []
-                for i in range(len(b) - len(a[1:])):
+                for i in range(len(b) - len(a)):
                     m1.append(0)
-                for i in a[1:]:
-                    m1.append(i)
-                m = subt(m2, m1, 1)
-                return m
-                
-        else:
-            m1 = [int(i) for i in a[1:]]
-            m2 = [int(i) for i in b]
-            if int(a) < int(b):
-                m = subt(m2, m1, 0)
-            else:
-                m = subt(m2, m1, 1)
-            return m
-    else:
-        print()
-        if len(b[1:]) != len(a):
-            if len(b[1:]) > len(a):
-                m1 = [int(i) for i in b[1:]]
-                m2 = []
-                for i in range(len(b[1:]) - len(a)):
-                    m2.append(0)
                 for i in a:
-                    m2.append(int(i))
-                m = subt(m1, m2, 0)
-                return m
-            else:
-                m2 = [int(i) for i in a]
-                m1 = []
-                for i in range(len(a) - len(b[1:])):
-                    m1.append(0)
-                for i in b[1:]:
                     m1.append(i)
                 m = subt(m2, m1, 1)
                 return m
         else:
-            m1 = [int(i) for i in b[1:]]
-            m2 = [int(i) for i in a]
-            if int(b) < int(a):
-                m = subt(m2, m1, 1)
+            m1 = [int(i) for i in a]
+            m2 = [int(i) for i in b]
+            if not srav(a, b):
+                m = subt(m1, m2, 0)
             else:
-                m = subt(m2, m1, 0)
+                m = subt(m2, m1, 1)
             return m
         
 # сложение в столбик
@@ -114,7 +91,7 @@ def addit(a, b):
     else:
         m1 = [int(i) for i in a]
         m2 = [int(i) for i in b]
-    m3 = [0] * (max(len(a), len(b)))
+    m3 = []
     v_ume = 0
     for i in range(len(m1) - 1, -1, -1):
         n = m1[i] + m2[i] + v_ume
@@ -123,30 +100,26 @@ def addit(a, b):
             while n - 10 >= 0:
                 n -= 10
                 v_ume += 1
-            m3[i] = n
+            m3.append(n)
         else:
-            m3[i] = n
-    return m3
+            m3.append(n)
+    if v_ume != 0:
+        m3.append(v_ume)
+    return m3[::-1]
 
 # проверка
-a = input()
-b = input()
-print(int(a) + int(b))
+a = input("Первое большое число: ")
+b = input("Второе большое число: ")
 start = datetime.datetime.now()
-if int(a) > 0 and int(b) > 0:
-    m = addit(a, b)
-    for i in m:
-        print(i, end='')
-elif int(a) < 0 and int(b) < 0:
-    m = addit(a[1:], b[1:])
-    print('-', end='')
-    for i in m:
-        print(i, end='')
-else:
-    m = subtraction(a, b)
-    for i in m:
-        print(i, end='')
-    
+print("Сложение: ", end='')
+m = addit(a, b)
+for i in m:
+    print(i, end='')
+print()
+print("Вычитание: ", end='')
+m = subtraction(a, b)
+for i in m:
+    print(i, end='')
 print()
 finish = datetime.datetime.now()
 print(finish - start)
