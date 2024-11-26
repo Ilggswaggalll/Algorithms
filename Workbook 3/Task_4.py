abebfb76398a2fd766641da2502158b55c758fda
Ilggswaggalll <162141227+Ilggswaggalll@users.noreply.github.com>
2024-11-26 11:22:54 +0300

    commit

---
+++ Workbook 3/Task_4.py
@@ -0,0 +1,29 @@
+def mul(a, b):
+    num_1 = a[::-1]
+    num_2 = b[::-1]
+
+    results = [0] * (len(num_1) + len(num_2))
+
+    for i in range(len(num_1)):
+        for j in range(len(num_2)):
+            results[i + j] += int(num_1[i]) * int(num_2[j])
+            print(results[i + j], i, j)
+
+    carry = 0
+    for i in range(len(results)):
+        results[i] += carry
+        print(results[i], end=' ')
+        carry = results[i] // 10
+        results[i] %= 10
+        print(carry, results[i])
+
+    while len(results) > 1 and results[-1] == 0:
+        results.pop()
+
+    return int(''.join(map(str, results[::-1])))
+
+
+# тест
+a = input("Первое большое число: ")
+b = input("Второе большое число: ")
+print(mul(a, b))
